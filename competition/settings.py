16f1e7c98d349088e7c05c2f37baad6e65572622
Armin <bigboss@diamond.dogs>
2025-07-05 14:08:30 +0330

    switched to redis

--- competition/settings.py
+++ competition/settings.py
@@ -56,7 +56,10 @@ ASGI_APPLICATION = 'competition.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [("127.0.0.1", 6379)]
+        }
     }
 }
 
